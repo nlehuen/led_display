38839c91d4cd160f434a9ae1cdf79f7f5eedcf8a
Nicolas Lehuen <nicolas@lehuen.com>
2012-09-05 23:52:54 +0200

    Initial works

---
+++ tweet.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import serial
+import time 
+
+try:
+    # in case of easy_installed PIL
+    import Image, ImageDraw, ImageFont
+except ImportError:
+    # in case of distribution or windows PIL
+    from PIL import Image, ImageDraw, ImageFont
+
+font = ImageFont.truetype("Minecraftia.ttf", 8)
+# font = ImageFont.truetype("uni05_53.ttf", 8)
+
+def wave(t, mod):
+    return abs(t%(mod*2-2)-mod+1)+1
+
+class Display(object):
+    def __init__(self, port='COM6:', baudrate=2000000):
+        self._serial = serial.Serial(port, baudrate, timeout=1)
+
+    def send_image(self, img):
+        data = [2, 1] + [(value==2 and 3 or value) for pixel in img.getdata() for value in pixel]
+        data = map(chr, data)
+        data = "".join(data)
+        self._serial.write(data)
+
+class TweetAnimation(object):
+    def __init__(self, tweet):
+        self._tweet = tweet
+        self._author = tweet['author']
+        self._author_size = font.getsize(self._author)
+        self._text = tweet['text']
+        self._text_size = font.getsize(self._text)
+
+    def draw(self, t):
+        img = Image.new("RGB", (32, 16))
+        draw = ImageDraw.Draw(img)
+        draw.text((32-wave(t,self._author_size[0]),-2), self._author, font=font, fill="#ff0002")
+        draw.text((32-wave(t,self._text_size[0]),6), self._text, font=font, fill="#ffffff")
+        return img
+
+if __name__ == '__main__':
+    tweet = dict(
+        author = u'@nlehuen',
+        text = u"Voix ambigüe d'un coeur qui au zéphyr préfère les jattes de kiwis. 1234567890"
+    )
+
+    display = Display()
+    animation = TweetAnimation(tweet)
+
+    t = 0
+    while True:
+        t = t + 1
+        img = animation.draw(t)    
+        display.send_image(img)
+        time.sleep(0.05)
