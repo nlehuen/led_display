ba0960b6a738c6049a6fddb5d08ea19609831bba
Nicolas Lehuen <nicolas@lehuen.com>
2012-09-15 16:27:12 +0200

    New configuration system + new structure for authentication configuration.

---
+++ configuration.py
@@ -0,0 +1,47 @@
+import json
+
+NOTHING = object()
+
+class Configuration(object):
+    @classmethod
+    def load(clazz, filename):
+        with open(filename) as configuration_file:
+            return Configuration(None, None, json.load(configuration_file))
+
+    def __init__(self, parent, name, value):
+        self._parent = parent
+        self._name = name
+        self._value = value
+
+    def __getitem__(self, key):
+        if isinstance(self._value, dict):
+            try:
+                return Configuration(self, key, self._value[key])
+            except KeyError:
+                pass
+        return Configuration(self, key, NOTHING)
+
+    def __getattr__(self, key):
+        return self.__getitem__(key)
+
+    def __name(self):
+        result = []
+        node = self
+        while node is not None:
+            if node._name is not None:
+                result.insert(0, node._name)
+            node = node._parent
+        return '.'.join(result)
+
+    def value(self, default=None):
+        if self._value is NOTHING:
+            return default
+        return self._value
+
+    def required(self):
+        if self._value is NOTHING:
+            raise Exception("%s is a required parameter" % self.__name())
+        return self._value
+
+    def exists(self):
+        return self._value is not NOTHING
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import traceback
 import json
 
+from configuration import Configuration
 import animator
 
 # Animations
@@ -21,9 +22,8 @@ except ImportError:
     animations.tweet = None
 
 if __name__ == '__main__':
-    # Configuration file
-    with open('configuration.json') as configuration_file:
-        configuration = json.load(configuration_file)
+    # Load configuration
+    configuration = Configuration.load('configuration.json')
 
     # Create display and animator
 
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     try:
         import leddisplay
         display = leddisplay.Display(
-            port = configuration['leddisplay']['port'].encode(),
-            speed = configuration['leddisplay']['speed'],
+            port = configuration.leddisplay.port.required().encode(),
+            speed = configuration.leddisplay.speed.required(),
             threaded = True
         )
     except:
@@ -42,34 +42,56 @@ if __name__ == '__main__':
         import tkdisplay
         display = tkdisplay.Display(
             (
-                configuration['tkdisplay']['width'],
-                configuration['tkdisplay']['height']
+                configuration.tkdisplay.width.value(32),
+                configuration.tkdisplay.height.value(16)
             ),
-            configuration['tkdisplay']['scale']
+            configuration.tkdisplay.scale.value(4)
         )
 
     animator = animator.Animator(
         display,
-        queue=configuration['animator']['queue'],
-        fps=configuration['animator']['fps'],
-        animation_timeout=configuration['animator']['timeout']
+        queue=configuration.animator.queue.value(120),
+        fps=configuration.animator.fps.value(25),
+        animation_timeout=configuration.animator.timeout.value(30)
     )
 
     # Animation queue
     if animations.tweet is not None:
-        animator.queue(animations.tweet.TweetAnimation(
+        twitter_auth = None
+
+        # First, try to use the basic authentication
+        auth_config = configuration.twitter.auth.basic
+        if auth_config.exists():
             twitter_auth = animations.tweet.UserPassAuth(
-                configuration['twitter']['login'],
-                configuration['twitter']['password']
-            ),
-            track = configuration['twitter']['track'],
-            fps = configuration['twitter'].get('fps', 0),
-            wait = configuration['twitter'].get('wait', 1),
-            speed = configuration['twitter'].get('speed', 1),
-            font = configuration['twitter'].get('font', 'alterebro-pixel-font.ttf'),
-            size = configuration['twitter'].get('size', 16),
-            baseline = configuration['twitter'].get('baseline', 4)
-        ))
+                auth_config.login.required(),
+                auth_config.password.required()
+            )
+
+        # If basic authentication was not found, try OAuth
+        if twitter_auth is None:
+            auth_config = configuration.twitter.auth.oauth
+            if auth_config.exists():
+                twitter_auth = animations.tweet.OAuth(
+                    auth_config.oauth_token.required(),
+                    auth_config.oauth_secret.required(),
+                    auth_config.consumer_key.required(),
+                    auth_config.consumer_secret.required(),
+                )
+
+        if twitter_auth is not None:
+            animator.queue(animations.tweet.TweetAnimation(
+                twitter_auth = twitter_auth,
+                track = configuration.twitter.track.value(),
+                fps = configuration.twitter.fps.value(0),
+                wait = configuration.twitter.wait.value(1),
+                speed = configuration.twitter.speed.value(1),
+                font = configuration.twitter.font.value('alterebro-pixel-font.ttf'),
+                size = configuration.twitter.size.value(16),
+                baseline = configuration.twitter.baseline.value(4)
+            ))
+        else:
+            print "You need to provide either twitter.auth.basic or"
+            print "twitter.auth.oauth authentication informations."
 
     # For the moment, run the animator in the main thread
     animator.mainloop()
